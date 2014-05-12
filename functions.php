ac31599e9ba3aed192910243acb9ef958b192129
Perryb <pbonewell@geemale.com>
2014-05-12 21:07:10 +0100

    First commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+add_action( 'after_setup_theme', 'childmaintenance_setup', 11 );
+
+function childmaintenance_setup() {
+
+	/* Get the parent theme prefix. */
+	$prefix = hybrid_get_prefix();
+
+	/* Actions and filters calls go here in the future. */
+}
