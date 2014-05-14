04e4f213972f2efcd9a7555f88bc99b163c8f4a0
Perry Bonewell <pbonewell@gmail.com>
2014-05-14 11:32:37 +0100

    Update content-page.php

--- content-page.php
+++ content-page.php
@@ -2,7 +2,7 @@
 /**
  * The template used for displaying page content in page.php
  *
- * @package childmaintenance
+ * @package parentalguidance
  */
 ?>
 
@@ -15,10 +15,10 @@
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'childmaintenance' ),
+				'before' => '<div class="page-links">' . __( 'Pages:', 'parentalguidance' ),
 				'after'  => '</div>',
 			) );
 		?>
 	</div><!-- .entry-content -->
-	<?php edit_post_link( __( 'Edit', 'childmaintenance' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer>' ); ?>
+	<?php edit_post_link( __( 'Edit', 'parentalguidance' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer>' ); ?>
 </article><!-- #post-## -->
