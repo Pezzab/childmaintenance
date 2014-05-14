d83a44b8ced90ae0d4c8922ac0449303294a40e5
Perry Bonewell <pbonewell@gmail.com>
2014-05-14 11:30:58 +0100

    Update content-single.php

--- content-single.php
+++ content-single.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package childmaintenance
+ * @package parentalguidance
  */
 ?>
 
@@ -9,7 +9,7 @@
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 
 		<div class="entry-meta">
-			<?php childmaintenance_posted_on(); ?>
+			<?php parentalguidance_posted_on(); ?>
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
@@ -17,7 +17,7 @@
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'childmaintenance' ),
+				'before' => '<div class="page-links">' . __( 'Pages:', 'parentalguidance' ),
 				'after'  => '</div>',
 			) );
 		?>
@@ -26,25 +26,25 @@
 	<footer class="entry-footer">
 		<?php
 			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'childmaintenance' ) );
+			$category_list = get_the_category_list( __( ', ', 'parentalguidance' ) );
 
 			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', __( ', ', 'childmaintenance' ) );
+			$tag_list = get_the_tag_list( '', __( ', ', 'parentalguidance' ) );
 
-			if ( ! childmaintenance_categorized_blog() ) {
+			if ( ! parentalguidance_categorized_blog() ) {
 				// This blog only has 1 category so we just need to worry about tags in the meta text
 				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'childmaintenance' );
+					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'parentalguidance' );
 				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'childmaintenance' );
+					$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'parentalguidance' );
 				}
 
 			} else {
 				// But this blog has loads of categories so we should probably display them here
 				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'childmaintenance' );
+					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'parentalguidance' );
 				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'childmaintenance' );
+					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'parentalguidance' );
 				}
 
 			} // end check for categories on this blog
@@ -57,6 +57,6 @@
 			);
 		?>
 
-		<?php edit_post_link( __( 'Edit', 'childmaintenance' ), '<span class="edit-link">', '</span>' ); ?>
+		<?php edit_post_link( __( 'Edit', 'parentalguidance' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
