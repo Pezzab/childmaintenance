9a8472ba026292d95f6d7e8d5bf0ce8a4fee539f
Perry Bonewell <pbonewell@gmail.com>
2014-05-14 11:22:26 +0100

    Update content.php

--- content.php
+++ content.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package childmaintenance
+ * @package parentalguidance
  */
 ?>
 
@@ -10,7 +10,7 @@
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php childmaintenance_posted_on(); ?>
+			<?php parentalguidance_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
@@ -22,17 +22,17 @@
 	<?php else : ?>
 	<div class="entry-content">
 		
-		<?php // the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'childmaintenance' ) ); ?>
+		<?php // the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'parentalguidance' ) ); ?>
 
 		<?php get_the_image( array( 'meta_key' => array( 'Thumbnail', 'thumbnail' ), 'image_class' => 'excerpt') ); ?>
 
-		<?php the_excerpt( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'childmaintenance' ) ); ?>
+		<?php the_excerpt( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'parentalguidance' ) ); ?>
 
 
 
 		<?php
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'childmaintenance' ),
+				'before' => '<div class="page-links">' . __( 'Pages:', 'parentalguidance' ),
 				'after'  => '</div>',
 			) );
 		?>
@@ -43,29 +43,29 @@
 		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
 			<?php
 				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'childmaintenance' ) );
-				if ( $categories_list && childmaintenance_categorized_blog() ) :
+				$categories_list = get_the_category_list( __( ', ', 'parentalguidance' ) );
+				if ( $categories_list && parentalguidance_categorized_blog() ) :
 			?>
 			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'childmaintenance' ), $categories_list ); ?>
+				<?php printf( __( 'Posted in %1$s', 'parentalguidance' ), $categories_list ); ?>
 			</span>
 			<?php endif; // End if categories ?>
 
 			<?php
 				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'childmaintenance' ) );
+				$tags_list = get_the_tag_list( '', __( ', ', 'parentalguidance' ) );
 				if ( $tags_list ) :
 			?>
 			<span class="tags-links">
-				<?php printf( __( 'Tagged %1$s', 'childmaintenance' ), $tags_list ); ?>
+				<?php printf( __( 'Tagged %1$s', 'parentalguidance' ), $tags_list ); ?>
 			</span>
 			<?php endif; // End if $tags_list ?>
 		<?php endif; // End if 'post' == get_post_type() ?>
 
 		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'childmaintenance' ), __( '1 Comment', 'childmaintenance' ), __( '% Comments', 'childmaintenance' ) ); ?></span>
+		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'parentalguidance' ), __( '1 Comment', 'parentalguidance' ), __( '% Comments', 'parentalguidance' ) ); ?></span>
 		<?php endif; ?>
 
-		<?php edit_post_link( __( 'Edit', 'childmaintenance' ), '<span class="edit-link">', '</span>' ); ?>
+		<?php edit_post_link( __( 'Edit', 'parentalguidance' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
