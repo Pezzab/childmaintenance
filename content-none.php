be4c454933b9be6e711ff8086db3ffe46bbde7a5
Perry Bonewell <pbonewell@gmail.com>
2014-05-14 11:32:05 +0100

    Update content-none.php

--- content-none.php
+++ content-none.php
@@ -4,28 +4,28 @@
  *
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
- * @package childmaintenance
+ * @package parentalguidance
  */
 ?>
 
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', 'childmaintenance' ); ?></h1>
+		<h1 class="page-title"><?php _e( 'Nothing Found', 'parentalguidance' ); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'childmaintenance' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'parentalguidance' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 		<?php elseif ( is_search() ) : ?>
 
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'childmaintenance' ); ?></p>
+			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'parentalguidance' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php else : ?>
 
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'childmaintenance' ); ?></p>
+			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'parentalguidance' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php endif; ?>
