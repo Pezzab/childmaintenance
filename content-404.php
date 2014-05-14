676155a04427b1c6c51295983482875cb57d51b3
Perry Bonewell <pbonewell@gmail.com>
2014-05-14 11:31:33 +0100

    Update content-404.php

--- content-404.php
+++ content-404.php
@@ -4,25 +4,25 @@
  *
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
- * @package childmaintenance
+ * @package parentalguidance
  */
 ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'childmaintenance' ); ?></h1>
+					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'parentalguidance' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'childmaintenance' ); ?></p>
+					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'parentalguidance' ); ?></p>
 
 					<?php get_search_form(); ?>
 
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-					<?php if ( childmaintenance_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+					<?php if ( parentalguidance_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
 					<div class="widget widget_categories">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'childmaintenance' ); ?></h2>
+						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'parentalguidance' ); ?></h2>
 						<ul>
 						<?php
 							wp_list_categories( array(
@@ -39,7 +39,7 @@
 
 					<?php
 					/* translators: %1$s: smiley */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'childmaintenance' ), convert_smilies( ':)' ) ) . '</p>';
+					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'parentalguidance' ), convert_smilies( ':)' ) ) . '</p>';
 					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 
 					?>
